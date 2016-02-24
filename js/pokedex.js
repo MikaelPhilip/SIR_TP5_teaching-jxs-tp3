6a4bce26c0c242a3db07b7be2b55eef04d72cedb
MikaelPhilip <philip.mikael@gmail.com>
2016-02-24 13:30:00 +0100

    recuperation d'un json pour liste de pokemon + ajout listener bouton go

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,5 +1,5 @@
 var pokeApp = angular.module('pokedex', ['ngResource'])
-					 .controller('ctrlPokemon', ['$scope', '$log',ctrlPokemon]); //Associé un controleur à une function qui représente son model
+					 .controller('ctrlPokemon', ctrlPokemon); //Associé un controleur à une function qui représente son model
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
@@ -9,17 +9,21 @@ var pokeApiUrl = "http://pokeapi.co/"
 
 
 //fonction associé à la ng-controller: va permettre d'associer un model au controleur
-function ctrlPokemon($scope){
-	//definition d'une liste de pokemon
-	var pok54 = {id:"54", nom:"Psykokwak", description:"blablabla", skills:["atk1","atk2","atk3"]}; 
-	var pok1 = {id:"1", nom:"Bulbizarre", description:"blablabla", skills:["atk1","atk2","atk3"]}; 
-	var pok4 = {id:"4", nom:"Salamèche", description:"blablabla", skills:["atk1","atk2","atk3"]}; 
-	var pok7 = {id:"7", nom:"Carapuce", description:"blablabla", skills:["atk1","atk2","atk3"]}; 
-	var pok19 = {id:"19", nom:"Rattata", description:"blablabla", skills:["atk1","atk2","atk3"]}; 
-	$scope.listPokemon = [pok54,pok1,pok4,pok7,pok19];
+function ctrlPokemon($scope, $http, $log){
+	//definition d'une liste de pokemon: on récupére la base des données
+	pokeApiUrlListTotal = pokeApiUrl + "api/v2/pokedex/1/"; 
 	
-	$scope.search = function() {
-		$scope.message = 'Hello World!';
+	//Requete pour récuperer une liste de pokemon
+    $http({
+        method: 'GET',
+        url: pokeApiUrlListTotal,
+    }).then(function successCallback(response) {
+			$scope.listPokemon= response.data.pokemon_entries
+    });
+	
+	  
+	$scope.search = function(item) {
+		$log.log($scope.listPokemon[item-1]);
 	}
 	
 }
